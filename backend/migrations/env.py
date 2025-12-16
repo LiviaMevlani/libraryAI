068cdc5f21f73c4e2b9a4aa8412dd62ce0d5f66e
Livia Mevlani <liviamevlani@Livias-MacBook-Pro.local>
2025-12-16 07:31:27 +0100

    Prepare for production deployment (Docker prod/dev split, nginx)

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -4,6 +4,11 @@ from logging.config import fileConfig
 from flask import current_app
 
 from alembic import context
+from extensions import db
+import models
+
+target_metadata = db.metadata
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
